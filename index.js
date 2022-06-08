45830cb330bd5652320e34b7263b8137294c4da6
Veronicah <veronicah.gicheru@student.moringaschool.com>
2022-06-08 15:54:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards( namesArray, event) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+}
+return thankYouCards
+}
+function countDown() {
+    let countDown = 10;
+    while ( countDown > -1 ) {
+        console.log(countDown--);
+    }
+    return countDown;
+}
